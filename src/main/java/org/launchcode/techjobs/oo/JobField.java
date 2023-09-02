c8957d9c4e1be882940d29a92942d56e7a8afbdb
Terri Penn <coderterri@protonmail.com>
2023-09-02 09:16:34 -0500

    Also made JobField.id final

--- src/main/java/org/launchcode/techjobs/oo/JobField.java
+++ src/main/java/org/launchcode/techjobs/oo/JobField.java
@@ -3,7 +3,7 @@ package org.launchcode.techjobs.oo;
 import java.util.Objects;
 
 public abstract class JobField {
-    private int id;
+    private final int id;
     private static int nextId = 1;
     private String value;
 
